b5ec2c5eed9ce51dfc4d68f52fddc180a79f40d6
Administrator <admin@example.com>
2022-12-15 15:08:16 +0200

    change

--- cluster.tf
+++ cluster.tf
@@ -6,7 +6,7 @@ module "cluster" {
   project         = var.project
   location        = "us-central1"
 
-  machine_type    = "n1-standard-2"
+  machine_type = "n1-standard-2"
 
   node_pool_name  = var.node_pool_name
   node_count      = var.nodes_number
--- sql.tf
+++ sql.tf
@@ -2,7 +2,7 @@ module "sql" {
   source            = "./modules/sql"
   db_name           = var.sql_name
   project           = var.project
-  DATABASE_NAME     = "${DATABASE_NAME}"
-  DATABASE_USER     = "${DATABASE_USER}"
-  DATABASE_PASSWORD = "${DATABASE_PASSWORD}"
+  DATABASE_NAME     = DATABASE_NAME
+  DATABASE_USER     = DATABASE_USER
+  DATABASE_PASSWORD = DATABASE_PASSWORD
 }
