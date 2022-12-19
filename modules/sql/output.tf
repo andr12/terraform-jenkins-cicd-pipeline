ea4a28a6df91733c7d758832d7a9e0bbedc36e55
Administrator <admin@example.com>
2022-12-19 16:48:34 +0200

    change

--- modules/sql/output.tf
+++ modules/sql/output.tf
@@ -1,4 +1,4 @@
 output "sql_connection_name" {
   description = "Connection name of the primary instance."
-  value       = google_sql_database_instance_instance.connection_name
+  value       = google_sql_database_instance.instance.connection_name
 }
